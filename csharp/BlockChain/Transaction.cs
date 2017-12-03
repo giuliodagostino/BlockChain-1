7b27f78d2de1a6b18d14dd549758bd31bc80ef71
Memetics Ireland Ltd <34136037+memeticsireland@users.noreply.github.com>
2017-12-03 19:08:59 +0000

    Create Transaction.cs

---
+++ csharp/BlockChain/Transaction.cs
@@ -0,0 +1,9 @@
+namespace BlockChainDemo
+{
+    public class Transaction
+    {
+        public int Amount { get; set; }
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+    }
+}
